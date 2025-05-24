70e47a76da16a19a27785b105665bedd1eb99b70
Harsika kumari <kumariharsika8@gmail.com>
2025-05-24 19:16:23 +0530

    feat: create user progress in course

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,16 +2,26 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const MainPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress 
-        activeCourse={{title: "Spanish", imageSrc: "/es.svg"}}
-        hearts={5}
-        points={100}
-        hasActiveSubscription={false}
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
@@ -21,4 +31,4 @@ const MainPage = () => {
   );
 };
 
-export default MainPage;
+export default LearnPage;
