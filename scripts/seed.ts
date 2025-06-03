76c37fd102aeeae479f56c17e03a18402e114d2e
Harsika kumari <kumariharsika8@gmail.com>
2025-06-03 11:21:54 +0530

    feat(seed): create seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,6 +12,11 @@ const main = async () => {
         console.log("Seeding the database...");
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
             {
@@ -38,6 +43,60 @@ const main = async () => {
                 id: 5,
                 title: "Hindi",
                 imageSrc: "/in.svg",
+            },
+        ]);
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1,
+                title: "Unit 1",
+                description: "Learn the basics of Spanish",
+                order: 1,
+            }
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1,
+                order: 1,
+                title: "Nouns",
+            },
+        ]);
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1,
+                type: "SELECT",
+                order: 1,
+                question: 'Which one of these is the "the man"?',
+            },
+        ]);
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 1,
+                challengeId: 1,
+                imgageSrc: "/man.svg",
+                correct: true,
+                text: "el hombre",
+                audionSrc: "/es_man.mp3"
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                imgageSrc: "/women.svg",
+                text: "la mujer",
+                audionSrc: "/es_woman.mp3"
+            },
+            {
+                id: 3,
+                challengeId: 1,
+                imgageSrc: "/robot.svg",
+                text: "el robot",
+                audionSrc: "/es_robot.mp3"
             },
         ]);
 
