c38dc40d7045aee06290906d42d8e07fda2630c8
Harsika kumari <kumariharsika8@gmail.com>
2025-06-16 11:44:41 +0530

    feat(ui): add lesson header

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { challenges, challengeOptions } from "@/db/schema";
+import { Header } from "./header";
+import { useState } from "react";
+
+type Props = {
+  initialPercentage: number;
+  initialHearts: number;
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  userSubscription: any; // Replace with subscription DB type
+};
+
+export const Quiz = ({
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
+}: Props) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
